292881f6f68e242a31d1d4adf35183e91ac4365b
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-03-08 11:41:13 +0530

    new section

--- reactTest/src/sections/CustomerReviews.jsx
+++ reactTest/src/sections/CustomerReviews.jsx
@@ -1,6 +1,18 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
+
 function CustomerReviews() {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="text-center font-palanquin text-4xl font-bold">What Our <span className="text-coral-red">Customers</span> Say?</h3>
+      <p className="mt-4 m-auto info-text max-w-lg text-center">Hear genuine stories from our satisfied customers about theri exceptional experiences with us.</p>
+
+      <div className="flex flex-1 justify-evenly mt-24 items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} imgURL={review.imgURL} customerName={review.customerName} rating={review.rating} feedback={review.feedback}/>
+        ))}
+      </div>
+    </section>
   )
 }
 
